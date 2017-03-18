016079e7e8b31f205b25ac8095c50f2dd4076cb9
Nathan Yam <nathancyam@gmail.com>
2017-03-18 21:06:09 +1100

    Use websockets instead of redis

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 'use strict';
 
-const ACTION_ADD_TORRENT = 'add_torrent';
-const ACTION_NEW_FILE = 'new_file';
-const ACTION_MOVE_TORRENT_FILE = 'move_torrent_file';
-const ACTION_PAUSE_TORRENT = 'pause_torrent';
-const ACTION_RESUME_TORRENT = 'resume_torrent';
-const ACTION_FORCE_UPDATE = 'force_update';
-
 const fs = require('fs');
 const path = require('path');
 const Redis = require('ioredis');
 const crypto = require('crypto');
 
 const config = JSON.parse(fs.readFileSync(__dirname + '/client.json').toString());
-const redisSub = new Redis(6379, config.redis.host);
+// const redisSub = new Redis(6379, config.redis.host);
+
 const updateUrl = config.update_url;
 const torrentUpdateUrl = config.torrent_server.update_url;
 const auth = config.auth;
@@ -31,24 +25,11 @@ const DownloadFileWatcher = require('./file_watcher').FileWatcher;
 const Rx = require('rx');
 
 const downloadFileWatcher = new DownloadFileWatcher(config.anime_directory);
-downloadFileWatcher.watch();
-
-var exec = require('child_process').exec;
-const execute = (cmd) => {
-  return new Promise((resolve, reject) => {
-    exec(cmd, (err, stdOut, stdErr) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(stdOut);
-    })
-  });
-};
 
 const source = Rx.Observable.fromEvent(downloadFileWatcher, 'move_file')
   .distinct()
   .subscribe(filename => {
-    var payload = {
+    const payload = {
       action: ACTION_NEW_FILE,
       filename: path.basename(filename),
     };
@@ -56,12 +37,6 @@ const source = Rx.Observable.fromEvent(downloadFileWatcher, 'move_file')
     console.log(`Request: ${updateUrl}: ${JSON.stringify(payload)}`);
     updateClient.postJson(updateUrl, auth, payload);
   });
-
-redisSub.subscribe('torrent', (err, count) => {
-  console.log(
-    `Subscribed to ${count} channels: ${config.redis.host}:6379. Listening on 'torrent' channel.`
-  );
-});
 
 let previousTorrentsHash = '';
 function postTorrentListing(response, forcePush) {
@@ -112,64 +87,20 @@ setInterval(() => {
   });
 }, 5000);
 
-redisSub.on('message', (channel, message) => {
-  if (channel !== 'torrent') {
-    return;
-  }
 
-  const payload = JSON.parse(message);
-  console.log(payload);
-  switch (payload.action) {
-    case ACTION_ADD_TORRENT:
-      let torrentUrl = payload.torrentUrl;
-      if (torrentUrl.indexOf('http:') !== 0) {
-        torrentUrl = `http:${torrentUrl}`;
-      }
-      torrentServer.add(torrentUrl, (err, res) => {
-        if (err) {
-          console.error(`Failed to add torrent: ${payload.torrentUrl} Error: ${err}`);
-        }
+const io = require('socket.io-client');
+console.info('Establishing WS connection with server');
 
-        console.log(`Torrent added successfully: ${payload.torrentUrl}`);
-      });
-      break;
-
-    case ACTION_NEW_FILE:
-      break;
-
-    case ACTION_MOVE_TORRENT_FILE:
-      fileMover.moveTorrentFiles(payload.torrentId, payload.destinationDirectory);
-      break;
-
-    case ACTION_PAUSE_TORRENT:
-      execute(`transmission-remote -t ${payload.torrentId} -S`)
-        .then(() => console.log(`Paused Torrent: ${payload.torrentId}`))
-        .catch(err => {
-          if (err) {
-            console.error(`Failed to stop torrent`);
-            console.error(err);
-          }
-        });
-      break;
-
-    case ACTION_RESUME_TORRENT:
-      execute(`transmission-remote -t ${payload.torrentId} -s`)
-        .then(() => console.log(`Started Torrent: ${payload.torrentId}`))
-        .catch(err => {
-          if (err) {
-            console.error(`Failed to stop torrent`);
-            console.error(err);
-          }
-        });
-      break;
-
-    case ACTION_FORCE_UPDATE:
-      torrentServer.get((err, response) => {
-        return postTorrentListing(response, true);
-      });
-      break;
-
-    default:
-      break;
-  }
-});
+const socketOptions = {
+  extraHeaders: {
+    'AnimeTorrentUser': config.socket.user,
+    'Authorization': config.socket.auth
+  },
+  reconnection: true,
+  reconnectionAttempts: 3,
+};
+const socket = io(config.socket.url, socketOptions);
+const socketHandler = require('./socket');
+socketHandler(socket, torrentServer, fileMover, postTorrentListing);
+console.info(`Starting download file child process`);
+downloadFileWatcher.watch();
