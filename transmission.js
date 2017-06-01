c0990623d1278a015f2020c3e7c0412af9c8fad2
Nathan Yam <nathancyam@gmail.com>
2017-06-01 22:06:19 +1000

    Wrap torrent file in quotation to resolve file

--- transmission.js
+++ transmission.js
@@ -5,11 +5,11 @@
 /*jslint node: true */
 "use strict";
 
-var async = require('async');
-var Q = require('q');
-var Transmission = require('transmission');
-var util = require('util');
-var fs = require('fs');
+const async = require('async');
+const Q = require('q');
+const Transmission = require('transmission');
+const util = require('util');
+const fs = require('fs');
 const exec = require('child_process').exec;
 const request = require('request');
 
@@ -18,23 +18,22 @@ const request = require('request');
  * @type {TransmissionWrapper}
  */
 
-var TransmissionWrapper = module.exports = function TransmissionWrapper(options) {
-    Transmission.call(this, options);
+const TransmissionWrapper = module.exports = function TransmissionWrapper(options) {
+  Transmission.call(this, options);
 };
 
 function execTorrent(filename, callback) {
-  const cmd = `transmission-remote -a ${filename}`;
+  const cmd = `transmission-remote -a "${filename}"`;
   console.log('cmd', cmd);
   exec(cmd, (error, stdout, stderr) => {
     if (error || stderr) {
-        console.error('Stderr', stderr);
+      console.error('Stderr', stderr);
       return callback(error);
     }
 
     console.log('Std out', stdout);
 
-
-   return callback(null, {
+    return callback(null, {
       status: 'fulfilled',
       message: `Torrent added ${filename}`,
       name: {
@@ -45,17 +44,17 @@ function execTorrent(filename, callback) {
 }
 
 function addTorrent(url, name, callback) {
-    if (url.includes('magnet')) {
-      return execTorrent(url, callback);
-    }
+  if (url.includes('magnet')) {
+    return execTorrent(url, callback);
+  }
 
-    const filename = `/var/torrents/${name}.torrent`;
-    const writeStream = fs.createWriteStream(filename);
-    request(url).pipe(writeStream);
+  const filename = `/var/torrents/${name}.torrent`;
+  const writeStream = fs.createWriteStream(filename);
+  request(url).pipe(writeStream);
 
-    writeStream.on('finish', () => {
-        execTorrent(filename, callback);
-    });
+  writeStream.on('finish', () => {
+    execTorrent(filename, callback);
+  });
 }
 
 /**
@@ -63,137 +62,138 @@ function addTorrent(url, name, callback) {
  *
  * @type {{formatTorrentResponses: formatTorrentResponses, formatResponseToJson: formatResponseToJson}}
  */
-var ResponseFormatter = {
-    /**
-     * Formats the results from the transmission torrent server to a presentable
-     * JSON format.
-     *
-     * @see Q.allSettled()
-     * @param responses Array of resolved promises from the Q.allSettled() method
-     */
-    formatTorrentResponses: function (responses) {
-        var successfulTorrents = [];
-        var failedTorrents = [];
+const ResponseFormatter = {
+  /**
+   * Formats the results from the transmission torrent server to a presentable
+   * JSON format.
+   *
+   * @see Q.allSettled()
+   * @param responses Array of resolved promises from the Q.allSettled() method
+   */
+  formatTorrentResponses: function (responses) {
+    const successfulTorrents = [];
+    const failedTorrents = [];
 
-        // Split the results to successful/failed additions
-        responses.forEach(function (torrent) {
-            switch (torrent.state) {
-                case 'fulfilled':
-                    successfulTorrents.push(torrent);
-                    break;
-                case 'rejected':
-                    failedTorrents.push(torrent);
-                    break;
-                default:
-                    break;
-            }
-        });
+    // Split the results to successful/failed additions
+    responses.forEach(function (torrent) {
+      switch (torrent.state) {
+        case 'fulfilled':
+          successfulTorrents.push(torrent);
+          break;
+        case 'rejected':
+          failedTorrents.push(torrent);
+          break;
+        default:
+          break;
+      }
+    });
 
-        var response = {
-            message: '',
-            responses: {
-                successful: this.formatResponseToJson('success', successfulTorrents),
-                failed: this.formatResponseToJson('error', failedTorrents)
-            }
-        };
+    const response = {
+      message: '',
+      responses: {
+        successful: this.formatResponseToJson('success', successfulTorrents),
+        failed: this.formatResponseToJson('error', failedTorrents)
+      }
+    };
 
-        if (successfulTorrents.length > 0 && failedTorrents.length === 0) {
-            response.message = "Successfully added all torrents to server.";
-        } else if (successfulTorrents.length > 0 && failedTorrents.length > 0) {
-            response.message = "Failed to add some torrents to the server.";
-        } else if (successfulTorrents.length === 0 && failedTorrents.length > 0) {
-            response.message = "Failed to add all torrents to the server.";
-        }
-
-        return response;
-    },
-    /**
-     * Formats the Transmission server responses to JSON.
-     *
-     * @param status String
-     * @param responses Array
-     */
-    formatResponseToJson: function (status, responses) {
-        var message = "%s %d torrent(s) to the torrent server.";
-
-        if ('success' === status) {
-            message = util.format(message, "Successfully added", responses.length);
-        }
-        if ('error' === status) {
-            message = util.format(message, "Failed to add", responses.length);
-        }
-
-        return {
-            message: message,
-            numberOfTorrents: responses.length,
-            torrents: responses.map(function (elem) {
-                return elem.value.name
-            })
-        }
+    if (successfulTorrents.length > 0 && failedTorrents.length === 0) {
+      response.message = "Successfully added all torrents to server.";
+    } else if (successfulTorrents.length > 0 && failedTorrents.length > 0) {
+      response.message = "Failed to add some torrents to the server.";
+    } else if (successfulTorrents.length === 0 && failedTorrents.length > 0) {
+      response.message = "Failed to add all torrents to the server.";
     }
+
+    return response;
+  },
+  /**
+   * Formats the Transmission server responses to JSON.
+   *
+   * @param status String
+   * @param responses Array
+   */
+  formatResponseToJson: function (status, responses) {
+    let message = "%s %d torrent(s) to the torrent server.";
+
+    if ('success' === status) {
+      message = util.format(message, "Successfully added", responses.length);
+    }
+    if ('error' === status) {
+      message = util.format(message, "Failed to add", responses.length);
+    }
+
+    return {
+      message: message,
+      numberOfTorrents: responses.length,
+      torrents: responses.map(function (elem) {
+        return elem.value.name
+      })
+    }
+  }
 };
 
 TransmissionWrapper.prototype = Object.create(Transmission.prototype, {
-    addMultipleTorrents: {
-        /**
-         * Adds multiple torrents to the Transmission server. Returns a promise containing
-         * the responses from the torrent server.
-         *
-         * @param torrents Array of torrent hyperlinks to be added to the torrent server
-         * @returns {Promise<Array>} A promise for the responses from the torrent server
-         */
-        value: function (torrents) {
-            // Create the promise to add an torrent
-            var self = this;
-            var deferred = Q.defer();
+  addMultipleTorrents: {
+    /**
+     * Adds multiple torrents to the Transmission server. Returns a promise containing
+     * the responses from the torrent server.
+     *
+     * @param torrents Array of torrent hyperlinks to be added to the torrent server
+     * @returns {Promise<Array>} A promise for the responses from the torrent server
+     */
+    value: function (torrents) {
+      // Create the promise to add an torrent
+      const self = this;
+      const deferred = Q.defer();
 
-            // Create an array of promises
-            var torrentPromiseArray = torrents.map(function (e) {
-                return self.enhancedAdd(e);
-            });
+      // Create an array of promises
+      const torrentPromiseArray = torrents.map(function (e) {
+        return self.enhancedAdd(e);
+      });
 
-            // Once the array of promises is fulfilled, we format them appropriately.
-            Q.allSettled(torrentPromiseArray).then(
-              function (results) {
-                  return deferred.resolve(ResponseFormatter.formatTorrentResponses(results));
-              },
-              function (err) {
-                  return deferred.reject(err);
-              }
-            );
+      // Once the array of promises is fulfilled, we format them appropriately.
+      Q.allSettled(torrentPromiseArray).then(
+        function (results) {
+          return deferred.resolve(ResponseFormatter.formatTorrentResponses(results));
+        },
+        function (err) {
+          return deferred.reject(err);
+        }
+      );
 
-            return deferred.promise;
-        }
-    },
-    add: {
-        value: function ({ torrentUrl, name }, options) {
-            return this.enhancedAdd(torrentUrl, name, options)
-        }
-    },
-    enhancedAdd: {
-        /**
-         * Wrapper to add torrents to the Transmission server. Handles errors slightly
-         * better to ensure we can see the URL was not successfully added to the torrent
-         * server.
-         *
-         * @param url
-         * @returns {*}
-         */
-        value: function (url, name) {
-            return new Promise((resolve, reject) => {
-              addTorrent(url, name, function (err, result) {
-                if (err) {
-                  var parseJsonErr = JSON.parse(err.result);
-                  parseJsonErr.arguments.url = url;
-                  err.result = JSON.stringify(parseJsonErr);
-                  return reject(err);
-                } else {
-                  return resolve(result);
-                }
-              });
-            });
-        }
+      return deferred.promise;
     }
+  },
+  add: {
+    value: function ({torrentUrl, name}, options) {
+      return this.enhancedAdd(torrentUrl, name, options)
+    }
+  },
+  enhancedAdd: {
+    /**
+     * Wrapper to add torrents to the Transmission server. Handles errors slightly
+     * better to ensure we can see the URL was not successfully added to the torrent
+     * server.
+     *
+     * @param {String} url
+     * @param {String} name
+     * @returns {*}
+     */
+    value: function (url, name) {
+      return new Promise((resolve, reject) => {
+        addTorrent(url, name, function (err, result) {
+          if (err) {
+            var parseJsonErr = JSON.parse(err.result);
+            parseJsonErr.arguments.url = url;
+            err.result = JSON.stringify(parseJsonErr);
+            return reject(err);
+          } else {
+            return resolve(result);
+          }
+        });
+      });
+    }
+  }
 });
 
 module.exports = TransmissionWrapper;
