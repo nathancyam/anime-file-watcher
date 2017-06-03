39bd60f0bfd68e0e491cf48d4226217ee22ac444
Nathan Yam <nathancyam@gmail.com>
2017-06-03 19:07:02 +1000

    Add extra options to socket for extra headers
    Mainly used for basic auth. Put these details in the config.json file. This also removes Redis references from the code.

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const ACTION_FORCE_UPDATE = 'force_update';
 
 const fs = require('fs');
 const path = require('path');
-const Redis = require('ioredis');
 const crypto = require('crypto');
 
 const config = JSON.parse(fs.readFileSync(__dirname + '/client.json').toString());
-const redisSub = new Redis(6379, config.redis.host);
 const io = require('socket.io-client');
 const updateUrl = config.update_url;
 const torrentUpdateUrl = config.torrent_server.update_url;
@@ -34,10 +32,10 @@ const Rx = require('rx');
 const downloadFileWatcher = new DownloadFileWatcher(config.anime_directory);
 downloadFileWatcher.watch();
 
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 const execute = (cmd) => {
   return new Promise((resolve, reject) => {
-    exec(cmd, (err, stdOut, stdErr) => {
+    exec(cmd, (err, stdOut) => {
       if (err) {
         return reject(err);
       }
@@ -46,10 +44,10 @@ const execute = (cmd) => {
   });
 };
 
-const source = Rx.Observable.fromEvent(downloadFileWatcher, 'move_file')
+Rx.Observable.fromEvent(downloadFileWatcher, 'move_file')
   .distinct()
   .subscribe(filename => {
-    var payload = {
+    const payload = {
       action: ACTION_NEW_FILE,
       filename: path.basename(filename),
     };
@@ -57,12 +55,6 @@ const source = Rx.Observable.fromEvent(downloadFileWatcher, 'move_file')
     console.log(`Request: ${updateUrl}: ${JSON.stringify(payload)}`);
     updateClient.postJson(updateUrl, auth, payload);
   });
-
-redisSub.subscribe('torrent', (err, count) => {
-  console.log(
-    `Subscribed to ${count} channels: ${config.redis.host}:6379. Listening on 'torrent' channel.`
-  );
-});
 
 let previousTorrentsHash = '';
 function postTorrentListing(response, forcePush) {
@@ -103,7 +95,6 @@ function postTorrentListing(response, forcePush) {
     updateClient.postJson(torrentUpdateUrl, auth, {
       torrentServer: simpleTorrents,
     });
-    return;
   }
 }
 
@@ -114,59 +105,64 @@ setInterval(() => {
   });
 }, 5000);
 
-const socket = io(config.socket.torrent_channel);
-socket.on('torrent', payload => {
+const socket = io(config.socket.torrent_channel, config.socket.options);
 
-  switch (payload.action) {
-    case ACTION_ADD_TORRENT:
-      /** @var {String} torrentUrl */
-      let torrentUrl = payload.torrentUrl;
-      torrentServer.add(payload)
-        .then(() => {
-          console.log(`Torrent added successfully: ${payload.name} - ${torrentUrl}`);
-        })
-        .catch(err => {
-          console.error(`Failed to add torrent: ${payload.name} -  ${torrentUrl} Error: ${err}`);
+socket.on('connect', () => {
+  console.log('Socket connected');
+
+  socket.on('torrent', payload => {
+    switch (payload.action) {
+      case ACTION_ADD_TORRENT:
+        /** @var {String} torrentUrl */
+        let torrentUrl = payload.torrentUrl;
+        torrentServer.add(payload)
+          .then(() => {
+            console.log(`Torrent added successfully: ${payload.name} - ${torrentUrl}`);
+          })
+          .catch(err => {
+            console.error(`Failed to add torrent: ${payload.name} -  ${torrentUrl} Error: ${err}`);
+          });
+
+        break;
+
+      case ACTION_NEW_FILE:
+        break;
+
+      case ACTION_MOVE_TORRENT_FILE:
+        fileMover.moveTorrentFiles(payload.torrentId, payload.destinationDirectory);
+        break;
+
+      case ACTION_PAUSE_TORRENT:
+        execute(`transmission-remote -t ${payload.torrentId} -S`)
+          .then(() => console.log(`Paused Torrent: ${payload.torrentId}`))
+          .catch(err => {
+            if (err) {
+              console.error(`Failed to stop torrent`);
+              console.error(err);
+            }
+          });
+        break;
+
+      case ACTION_RESUME_TORRENT:
+        execute(`transmission-remote -t ${payload.torrentId} -s`)
+          .then(() => console.log(`Started Torrent: ${payload.torrentId}`))
+          .catch(err => {
+            if (err) {
+              console.error(`Failed to stop torrent`);
+              console.error(err);
+            }
+          });
+        break;
+
+      case ACTION_FORCE_UPDATE:
+        torrentServer.get((err, response) => {
+          return postTorrentListing(response, true);
         });
+        break;
 
-      break;
-
-    case ACTION_NEW_FILE:
-      break;
-
-    case ACTION_MOVE_TORRENT_FILE:
-      fileMover.moveTorrentFiles(payload.torrentId, payload.destinationDirectory);
-      break;
-
-    case ACTION_PAUSE_TORRENT:
-      execute(`transmission-remote -t ${payload.torrentId} -S`)
-        .then(() => console.log(`Paused Torrent: ${payload.torrentId}`))
-        .catch(err => {
-          if (err) {
-            console.error(`Failed to stop torrent`);
-            console.error(err);
-          }
-        });
-      break;
-
-    case ACTION_RESUME_TORRENT:
-      execute(`transmission-remote -t ${payload.torrentId} -s`)
-        .then(() => console.log(`Started Torrent: ${payload.torrentId}`))
-        .catch(err => {
-          if (err) {
-            console.error(`Failed to stop torrent`);
-            console.error(err);
-          }
-        });
-      break;
-
-    case ACTION_FORCE_UPDATE:
-      torrentServer.get((err, response) => {
-        return postTorrentListing(response, true);
-      });
-      break;
-
-    default:
-      break;
-  }
+      default:
+        break;
+    }
+  });
 });
+
